685d24b8a0b4ba9d1e2b9c849f0798548e96253a
Brad <bradley.hounkpatin@gmail.com>
2019-01-16 22:54:22 -0600

    letter pattern request is now made in one call

--- components/RandomPractice.js
+++ components/RandomPractice.js
@@ -132,85 +132,15 @@ function goToNextRandomWord(){
         apiResponse = response.data        
         numberOfDefinitions = apiResponse.results.length
         dataGoingToStore = {}
-        console.log('FIRST RESPONSE HAS COME');
         
-        switch (getPracticeMode()) {
-            case 'letterPattern':
-                console.log('LETTER PATTERN CASE');
-                let numberOfPages = ( apiResponse.results.total%100 === 0 ? apiResponse.results.total/100 : Math.floor((apiResponse.results.total/100)+1))
-                console.log('NUMBER OF PAGES : ', numberOfPages);
-                let apiRequestLetterPattern = null
-                let apiRequestRandomWord = null
-                if(numberOfPages > 1) {
-                    console.log('NUMBER OF PAGES GREATER THAN 1');
-                    let randomPage = Math.floor((Math.random() * numberOfPages) + 1)
-                    apiRequestLetterPattern = axios.create({
-                        baseURL: getBaseUrl() + '&page=' +randomPage.toString(),
-                        headers: {
-                            'X-Mashape-Key': AppConstants.WORDS_API_KEY,
-                            'Accept': 'application/json',
-                            'Content-Type': 'application/json'
-                        }
-                    })
-                    apiRequestLetterPattern.get()
-                    .then((response) => {
-                        let wordHasDefinition = false
-                        let randomWordIndex = null
-                        let numberOfWordsOnPage = response.data.results.data.length
-                        do {
-                            randomWordIndex = Math.floor((Math.random() * numberOfWordsOnPage))
-                            apiRequestRandomWord = axios.create({
-                                baseURL: 'https://wordsapiv1.p.mashape.com/words/' + response.data.results.data[randomWordIndex] + '/definitions',
-                                headers: {
-                                    'X-Mashape-Key': AppConstants.WORDS_API_KEY,
-                                    'Accept': 'application/json',
-                                    'Content-Type': 'application/json'
-                                }
-                            })
-                            apiRequestRandomWord.get()
-                            .then((response) => {
-                                if(response.data.definitions.length !== 0) {
-                                    wordHasDefinition = true
-                                    apiRequestRandomWord = axios.create({
-                                        baseURL: 'https://wordsapiv1.p.mashape.com/words/' + response.data.word,
-                                        headers: {
-                                            'X-Mashape-Key': AppConstants.WORDS_API_KEY,
-                                            'Accept': 'application/json',
-                                            'Content-Type': 'application/json'
-                                        }
-                                    })
-                                    apiRequestRandomWord.get()
-                                    .then((response) => {
-                                        console.log(response)
-                                        dataGoingToStore = {
-                                            word: response.data.word,
-                                            partOfSpeech: (response.data.results[0].partOfSpeech ? response.data.results[0].partOfSpeech : 'empty'),
-                                            pronunciation: (response.data.pronunciation ? (response.data.pronunciation.all ? response.data.pronunciation.all : 'empty') : 'empty'),
-                                            frequency: (response.data.frequency ? response.data.frequency.toString() : 'empty'),
-                                            definition: response.data.results[0].definition,
-                                        }
-                                        store.dispatch(addResponseData(dataGoingToStore))    
-                                    })
-                                }
-                            })
-                        } while (wordHasDefinition);
-                    })
-                }
-                break;
-        
-            default:
-                console.log('DEFAULT CASE');
-                
-                dataGoingToStore = {
-                    word: apiResponse.word,
-                    partOfSpeech: (apiResponse.results[0].partOfSpeech ? apiResponse.results[0].partOfSpeech : 'empty'),
-                    pronunciation: (apiResponse.pronunciation ? (apiResponse.pronunciation.all ? apiResponse.pronunciation.all : 'empty') : 'empty'),
-                    frequency: (apiResponse.frequency ? apiResponse.frequency.toString() : 'empty'),
-                    definition: apiResponse.results[0].definition,
-                }
-                store.dispatch(addResponseData(dataGoingToStore)) 
-                break;
+        dataGoingToStore = {
+        word: apiResponse.word,
+        partOfSpeech: (apiResponse.results[0].partOfSpeech ? apiResponse.results[0].partOfSpeech : 'empty'),
+        pronunciation: (apiResponse.pronunciation ? (apiResponse.pronunciation.all ? apiResponse.pronunciation.all : 'empty') : 'empty'),
+        frequency: (apiResponse.frequency ? apiResponse.frequency.toString() : 'empty'),
+        definition: apiResponse.results[0].definition,
         }
+        store.dispatch(addResponseData(dataGoingToStore)) 
     })
     .catch((error) => console.error(error))
 }
@@ -247,7 +177,7 @@ function getBaseUrl() {
             return 'https://wordsapiv1.p.mashape.com/words/example'
 
         case 'letterPattern':
-            return 'https://wordsapiv1.p.mashape.com/words/?letterPattern=^.{5}$&hasDetails=definitions'
+            return 'https://wordsapiv1.p.mashape.com/words/?letterPattern=^.{5}$&hasDetails=definitions&random=true'
 
         default:
             return 'https://wordsapiv1.p.mashape.com/words/?hasDetails=definitions&random=true'
