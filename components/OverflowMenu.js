5aa2cbc24654b333aad39cdc4f074d58f958d1c2
Brad <bradley.hounkpatin@gmail.com>
2019-01-31 11:55:34 -0600

    removed home menu item from home screen

--- components/OverflowMenu.js
+++ components/OverflowMenu.js
@@ -14,7 +14,6 @@ export const HomeOverflowMenu = (props) => {
         <Icon name='more-vert' color={AppConstants.COLOR_WHITE} />
          </MenuTrigger>
          <MenuOptions>
-          <MenuOption text={AppConstants.STRING_TAB_HOME} onSelect={() => props.navigation.navigate('Home')}/>
           <MenuOption text={AppConstants.STRING_TAB_MY_VOCABULARY} onSelect={() => props.navigation.navigate('MyVocabulary')}/>
           <MenuOption text={AppConstants.STRING_TAB_SETTINGS} onSelect={() => props.navigation.navigate('Settings')}/>
           <MenuOption text={AppConstants.STRING_ABOUT} />
