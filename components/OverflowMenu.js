5fdb7144daae3ba1b38b310dcce29dbea503fd06
Brad <bradley.hounkpatin@gmail.com>
2019-01-31 04:00:52 -0600

    added home and my vocabulary to overflow menu options

--- components/OverflowMenu.js
+++ components/OverflowMenu.js
@@ -14,16 +14,17 @@ export default function OverflowMenu(props) {
         <Icon name='more-vert' color={AppConstants.COLOR_WHITE} />
          </MenuTrigger>
          <MenuOptions>
-           <MenuOption text={AppConstants.STRING_PREFERENCES} onSelect={() => props.navigation.navigate('Settings')}/>
-           <MenuOption text={AppConstants.STRING_ABOUT} />
-           <MenuOption text={AppConstants.STRING_SIGN_OUT} onSelect={() => onSignOutSelected(props.navigation)} />
+          <MenuOption text={AppConstants.STRING_TAB_HOME} onSelect={() => props.navigation.navigate('Home')}/>
+          <MenuOption text={AppConstants.STRING_TAB_MY_VOCABULARY} onSelect={() => props.navigation.navigate('MyVocabulary')}/>
+          <MenuOption text={AppConstants.STRING_TAB_SETTINGS} onSelect={() => props.navigation.navigate('Settings')}/>
+          <MenuOption text={AppConstants.STRING_ABOUT} />
+          <MenuOption text={AppConstants.STRING_SIGN_OUT} onSelect={() => onSignOutSelected()} />
          </MenuOptions>
       </Menu>
     </View>
     )
   }
   
-  function onSignOutSelected(navigation) {
+  function onSignOutSelected() {
     firebase.auth().signOut()
-    // navigation.navigate('LoginScreen')
   }
