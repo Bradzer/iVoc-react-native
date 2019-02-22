58c3ea627a8cea48edb103db76b1ebfdba2576aa
Brad <bradley.hounkpatin@gmail.com>
2019-02-22 13:21:14 -0400

    commented reactotron redux occurences

--- ReactotronConfig.js
+++ ReactotronConfig.js
@@ -1,5 +1,5 @@
 import Reactotron from 'reactotron-react-native'
-import { reactotronRedux } from 'reactotron-redux'
+// import { reactotronRedux } from 'reactotron-redux'
 
 const reactotron = Reactotron
   .configure({host: '192.168.0.14'}) // controls connection & communication settings
