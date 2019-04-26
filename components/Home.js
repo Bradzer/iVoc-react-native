e26fd59c2039eaffd1f402397c08cd539086bca3
Levi Buzolic <levibuzolic@gmail.com>
2019-04-26 15:29:07 +1000

    Refactor

--- components/Home.js
+++ components/Home.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import { StyleSheet, View, BackHandler, } from 'react-native';
-import { Button, } from 'react-native-elements'
+import { StyleSheet, View, BackHandler } from 'react-native';
+import { Button } from 'react-native-elements'
 import firebase from 'react-native-firebase'
 import { inject, observer } from 'mobx-react'
 import { autorun } from 'mobx'
+import { NavigationEvents } from 'react-navigation'
 
 import {HomeOverflowMenu} from './OverflowMenu'
 import AppConstants from '../Constants'
@@ -15,9 +16,6 @@ class Home extends React.Component {
     myAutorun = autorun(() => {
     })
 
-
-    _didFocusSubscription = null;
-    _willBlurSubscription = null;
     _authStateListener = null;
 
     static navigationOptions = ({navigation}) => {
@@ -25,44 +23,44 @@ class Home extends React.Component {
             headerTitle: AppConstants.APP_NAME,
             headerStyle: {
                 backgroundColor: AppConstants.APP_PRIMARY_COLOR
-              },
-              headerTintColor: AppConstants.COLOR_WHITE,
-              headerRight: <HomeOverflowMenu navigation={navigation} />        }
+            },
+            headerTintColor: AppConstants.COLOR_WHITE,
+            headerRight: <HomeOverflowMenu navigation={navigation} />
+        }
     }
 
     render() {
         return(
             <View style={styles.container}>
                 <View style={styles.buttonGroup}>
-                    <Button 
-                        raised 
-                        titleStyle={{ fontSize: 24}} 
-                        buttonStyle={{maxWidth: 250}} 
-                        containerStyle={{marginVertical: 16}} 
+                    <Button
+                        raised
+                        titleStyle={{ fontSize: 24}}
+                        buttonStyle={{maxWidth: 250}}
+                        containerStyle={{marginVertical: 16}}
                         title={AppConstants.STRING_START_RANDOM_PRACTICE}
                         onPress={() => this.props.navigation.navigate('RandomPractice')}
-                        />
-                    <Button 
-                    titleStyle={{ fontSize: 24}} 
-                    buttonStyle={{maxWidth: 250}} 
-                    title={AppConstants.STRING_REVIEW_MY_VOCABULARY}
-                    onPress={() => this.props.navigation.navigate('ReviewVocabulary')}
+                    />
+                    <Button
+                        titleStyle={{ fontSize: 24}}
+                        buttonStyle={{maxWidth: 250}}
+                        title={AppConstants.STRING_REVIEW_MY_VOCABULARY}
+                        onPress={() => this.props.navigation.navigate('ReviewVocabulary')}
                     />
                 </View>
+                <NavigationEvents
+                    onDidFocus={() => {
+                        BackHandler.addEventListener('hardwareBackPress', this.onBackButtonPressAndroid)
+                    }}
+                    onWillBlur={() => {
+                        BackHandler.removeEventListener('hardwareBackPress', this.onBackButtonPressAndroid)
+                    }}
+                />
             </View>
         )
     }
 
     componentDidMount() {
-
-        this._didFocusSubscription = this.props.navigation.addListener('didFocus', () => {
-        BackHandler.addEventListener('hardwareBackPress', this.onBackButtonPressAndroid)
-        });
-
-        this._willBlurSubscription = this.props.navigation.addListener('willBlur', () =>
-        BackHandler.removeEventListener('hardwareBackPress', this.onBackButtonPressAndroid)
-        );
-
         this._authStateListener = firebase.auth().onAuthStateChanged((user) => {
             if(!user) {
                 this.props.navigation.navigate('LoginScreen')
@@ -77,10 +75,8 @@ class Home extends React.Component {
           return false;
         }
       };
-    
+
     componentWillUnmount() {
-        this._didFocusSubscription && this._didFocusSubscription.remove();
-        this._willBlurSubscription && this._willBlurSubscription.remove();
         this._authStateListener()
         this.myAutorun()
     }
