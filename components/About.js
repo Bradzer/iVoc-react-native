b0ca1bce207ed98f08952b03bb6236db0d73b23f
Brad <bradley.hounkpatin@gmail.com>
2019-07-31 14:12:59 -0300

    URLs can now be directly open

--- components/About.js
+++ components/About.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, ScrollView, View, Text, BackHandler } from 'react-native';
+import { StyleSheet, ScrollView, View, Text, BackHandler, ToastAndroid, Linking } from 'react-native';
 import { Button, } from 'react-native-elements'
 import { StackActions } from 'react-navigation'
 
@@ -39,9 +39,9 @@ class About extends React.Component {
                         <Text style={{marginTop: 16, fontWeight: 'bold', textDecorationLine: 'underline', fontSize: 18}}>{AppConstants.STRING_EMAIL}</Text>
                         <Text style={{fontSize: 18}}>{AppConstants.STRING_EMAIL_VALUE}</Text>
                         <Text style={{marginTop: 16, fontWeight: 'bold', textDecorationLine: 'underline', fontSize: 18}}>{AppConstants.STRING_PROJECT_REPO}</Text>
-                        <Text style={{fontSize: 18}}>{AppConstants.STRING_PROJECT_REPO_LINK}</Text>
+                        <Text style={{fontSize: 18}} onPress={() => this.onUrlPressed(AppConstants.STRING_PROJECT_REPO_LINK)}>{AppConstants.STRING_PROJECT_REPO_LINK}</Text>
                         <Text style={{marginTop: 16, fontWeight: 'bold', textDecorationLine: 'underline', fontSize: 18}}>{AppConstants.STRING_DEVELOPER_GITHUB}</Text>
-                        <Text style={{fontSize: 18}}>{AppConstants.STRING_DEVELOPER_GITHUB_LINK}</Text>
+                        <Text style={{fontSize: 18}} onPress={() => this.onUrlPressed(AppConstants.STRING_DEVELOPER_GITHUB_LINK)}>{AppConstants.STRING_DEVELOPER_GITHUB_LINK}</Text>
                         <Button title={AppConstants.STRING_USED_LIBRARIES} containerStyle={{marginTop: 16}} onPress={this.showLibrariesPressed}/>
                         <Text style={{marginTop: 16, fontWeight: 'bold', textDecorationLine: 'underline', fontSize: 18}}>{AppConstants.STRING_BIG_THANKS}</Text>
                         <Text style={{fontSize: 18}}>{AppConstants.STRING_AMIROL}</Text>
@@ -56,23 +56,23 @@ class About extends React.Component {
                     (
                     <View style={{flex: 1, alignItems: 'center'}}>
                         <Text style={{marginTop: 16, fontWeight: 'bold', textDecorationLine: 'underline', fontSize: 18}}>{AppConstants.STRING_AXIOS}</Text>
-                        <Text style={{fontSize: 18}}>{AppConstants.STRING_AXIOS_LINK}</Text>
+                        <Text style={{fontSize: 18}} onPress={() => this.onUrlPressed(AppConstants.STRING_AXIOS_LINK)}>{AppConstants.STRING_AXIOS_LINK}</Text>
                         <Text style={{marginTop: 16, fontWeight: 'bold', textDecorationLine: 'underline', fontSize: 18}}>{AppConstants.STRING_MOBX}</Text>
-                        <Text style={{fontSize: 18}}>{AppConstants.STRING_MOBX_LINK}</Text>
+                        <Text style={{fontSize: 18}} onPress={() => this.onUrlPressed(AppConstants.STRING_MOBX_LINK)}>{AppConstants.STRING_MOBX_LINK}</Text>
                         <Text style={{marginTop: 16, fontWeight: 'bold', textDecorationLine: 'underline', fontSize: 18}}>{AppConstants.STRING_REACT_NATIVE_ELEMENTS}</Text>
-                        <Text style={{fontSize: 18}}>{AppConstants.STRING_REACT_NATIVE_ELEMENTS_LINK}</Text>
+                        <Text style={{fontSize: 18}} onPress={() => this.onUrlPressed(AppConstants.STRING_REACT_NATIVE_ELEMENTS_LINK)}>{AppConstants.STRING_REACT_NATIVE_ELEMENTS_LINK}</Text>
                         <Text style={{marginTop: 16, fontWeight: 'bold', textDecorationLine: 'underline', fontSize: 18}}>{AppConstants.STRING_REACT_NATIVE_FIREBASE}</Text>
-                        <Text style={{fontSize: 18}}>{AppConstants.STRING_REACT_NATIVE_FIREBASE_LINK}</Text>
+                        <Text style={{fontSize: 18}} onPress={() => this.onUrlPressed(AppConstants.STRING_REACT_NATIVE_FIREBASE_LINK)}>{AppConstants.STRING_REACT_NATIVE_FIREBASE_LINK}</Text>
                         <Text style={{marginTop: 16, fontWeight: 'bold', textDecorationLine: 'underline', fontSize: 18}}>{AppConstants.STRING_REACT_NATIVE_INDICATORS}</Text>
-                        <Text style={{fontSize: 18}}>{AppConstants.STRING_REACT_NATIVE_INDICATORS_LINK}</Text>
+                        <Text style={{fontSize: 18}} onPress={() => this.onUrlPressed(AppConstants.STRING_REACT_NATIVE_INDICATORS_LINK)}>{AppConstants.STRING_REACT_NATIVE_INDICATORS_LINK}</Text>
                         <Text style={{marginTop: 16, fontWeight: 'bold', textDecorationLine: 'underline', fontSize: 18}}>{AppConstants.STRING_REACT_NATIVE_POPUP_MENU}</Text>
-                        <Text style={{fontSize: 18}}>{AppConstants.STRING_REACT_NATIVE_POPUP_MENU_LINK}</Text>
+                        <Text style={{fontSize: 18}} onPress={() => this.onUrlPressed(AppConstants.STRING_REACT_NATIVE_POPUP_MENU_LINK)}>{AppConstants.STRING_REACT_NATIVE_POPUP_MENU_LINK}</Text>
                         <Text style={{marginTop: 16, fontWeight: 'bold', textDecorationLine: 'underline', fontSize: 18}}>{AppConstants.STRING_REACT_NATIVE_VECTOR_ICONS}</Text>
-                        <Text style={{fontSize: 18}}>{AppConstants.STRING_REACT_NATIVE_VECTOR_ICONS_LINK}</Text>
+                        <Text style={{fontSize: 18}} onPress={() => this.onUrlPressed(AppConstants.STRING_REACT_NATIVE_VECTOR_ICONS_LINK)}>{AppConstants.STRING_REACT_NATIVE_VECTOR_ICONS_LINK}</Text>
                         <Text style={{marginTop: 16, fontWeight: 'bold', textDecorationLine: 'underline', fontSize: 18}}>{AppConstants.STRING_REACT_NAVIGATION}</Text>
-                        <Text style={{fontSize: 18}}>{AppConstants.STRING_REACT_NAVIGATION_LINK}</Text>
+                        <Text style={{fontSize: 18}} onPress={() => this.onUrlPressed(AppConstants.STRING_REACT_NAVIGATION_LINK)}>{AppConstants.STRING_REACT_NAVIGATION_LINK}</Text>
                         <Text style={{marginTop: 16, fontWeight: 'bold', textDecorationLine: 'underline', fontSize: 18}}>{AppConstants.STRING_REALM}</Text>
-                        <Text style={{fontSize: 18}}>{AppConstants.STRING_REALM_LINK}</Text>
+                        <Text style={{fontSize: 18}} onPress={() => this.onUrlPressed(AppConstants.STRING_REALM_LINK)}>{AppConstants.STRING_REALM_LINK}</Text>
                     </View>
                     )}
                 </ScrollView>
@@ -106,6 +106,18 @@ class About extends React.Component {
 
     showLibrariesPressed = () => {
         this.setState({displayLibraries: true})
+    }
+
+    onUrlPressed = (url) => {
+        Linking.canOpenURL(url)
+            .then((supported) => {
+                if (!supported) {
+                    ToastAndroid.show("No app installed to handle url", ToastAndroid.SHORT)
+                } else {
+                return Linking.openURL(url);
+                }
+            })
+            .catch(() => ToastAndroid.show(AppConstants.TOAST_ERROR, ToastAndroid.SHORT));
     }
 }
 
